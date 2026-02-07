b36891a83214bf1dc74558205e2d5b1ff8f9f193
Abdul Haseeb <abdull.haseebkhan@gmail.com>
2026-02-07 22:44:34 +0000

    Testing Cookie reading from Header

--- Internal/Middleware/Middlewar.go
+++ Internal/Middleware/Middlewar.go
@@ -22,16 +22,23 @@ func Authorizer(storage Storage.SysAdmin, next http.HandlerFunc) http.HandlerFun
 		sessionCookie := sessionToken.Value
 		slog.Info("Session Cookie Recieved", "ok:", sessionCookie)
 
-		csrfCookie, cserr := r.Cookie("csrf_token")
+		csrfCookie := r.Header.Get("X-CSRF-Token")
 
-		if cserr != nil {
-			slog.Info("CSRF Cookie Missing", "ok:", sessionToken)
+		// if cserr != nil {
+		// 	slog.Info("CSRF Cookie Missing", "ok:", sessionToken)
+		// 	response.WriteJson(w, http.StatusUnauthorized, response.GeneralError(errors.New("unauthorized Access")))
+		// 	return
+		// }
+
+		if csrfCookie == "" {
+			slog.Info("CSRF Cookie Missing", "error:", csrfCookie)
 			response.WriteJson(w, http.StatusUnauthorized, response.GeneralError(errors.New("unauthorized Access")))
 			return
 		}
+
 		slog.Info("CSRF Cookie Recieved", "ok:", csrfCookie)
 
-		id, isAuthorised := storage.AuthorizeSysAdmin(r.Context(), sessionCookie, csrfCookie.Value)
+		id, isAuthorised := storage.AuthorizeSysAdmin(r.Context(), sessionCookie, csrfCookie)
 		if !isAuthorised {
 			slog.Info("sessionCokie & CSRFToken mismatch", "error:", "unauthorized Access")
 			response.WriteJson(w, http.StatusUnauthorized, response.GeneralError(errors.New("unauthorized Access")))
