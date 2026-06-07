f01925de01d10ba2366d58c529fe104c1a1039ad
Abdul Haseeb <abdull.haseebkhan@gmail.com>
2026-06-07 18:08:50 +0100

    Tgl Expression for doc upload Fix 2

--- Internal/Storage/TGL/TGLStorage.go
+++ Internal/Storage/TGL/TGLStorage.go
@@ -99,7 +99,7 @@ func (p *TglAppStore) TglUploadProduct(ctx context.Context, product Types.TglPro
 		)
 		VALUES (
 			$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,
-			$11,$12,$13,$14,$15,$16,$17,$18,$19,20
+			$11,$12,$13,$14,$15,$16,$17,$18,$19,$20
 		)
 	`,
 		product.SKU, product.Name, product.EANBarcode, product.UPCBarcode,
