63375322d343761bfde111dc514409f0f526bdcd
Abdul Haseeb <abdull.haseebkhan@gmail.com>
2026-09-21 22:50:52 +0100

    changed AWS SDK region

--- Internal/Secrets/secrets.go
+++ Internal/Secrets/secrets.go
@@ -19,7 +19,7 @@ type SecretsFetcher struct {
 }
 
 func NewSecretsFetcher() (*SecretsFetcher, error) {
-	awsCfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	awsCfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-west-2"))
 	if err != nil {
 		return nil, fmt.Errorf("failed to load AWS config: %w", err)
 	}
